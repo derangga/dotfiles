6219ce13ad839c3cf54d988fd51d81e4822389fd
derangga <derangga1011@gmail.com>
2026-05-02 00:17:07 +0700

    chore: cloudflared and lsp neovim remove nil_ls

--- flake.nix
+++ flake.nix
@@ -68,7 +68,5 @@
         username = "sociolla";
       };
 
-      # Default package output for personal laptop
-      darwinPackages = self.darwinConfigurations."maclop".pkgs;
     };
 }
--- modules/hosts/maclop.nix
+++ modules/hosts/maclop.nix
@@ -4,8 +4,9 @@
 }:
 {
   home.packages = with pkgs; [
-    cloudflared
     openscreen
+    cocoapods
+    flutter
   ];
 
   catppuccin.obs = {
